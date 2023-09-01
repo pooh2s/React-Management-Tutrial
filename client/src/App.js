de48f6277300c55ff6de58286c970288193f96b0
LSH <leesh961028@gmail.com>
2023-09-01 18:44:14 +0900

    Add CustomerDelete Module

--- client/src/App.js
+++ client/src/App.js
@@ -81,6 +81,7 @@ class App extends Component {
                 <TableCell>생년월일</TableCell>
                 <TableCell>성별</TableCell>
                 <TableCell>직업</TableCell>
+                <TableCell>설정</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -88,6 +89,7 @@ class App extends Component {
                 this.state.customers ? this.state.customers.map(c => {
                   return (
                     <Customer
+                      stateRefresh={this.stateRefresh}
                       key={c.id}
                       id={c.id}
                       image={c.image}
